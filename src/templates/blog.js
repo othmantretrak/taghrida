6711de62160b0a077eee5e9b70226c9930c70ba3
tretrak <3trak10@gmail.com>
2019-05-28 21:33:17 +0100

    fix fiddxjj

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -99,7 +99,7 @@ const Blog = props => {
           <div className="ads1">
             <ins
               className="adsbygoogle"
-              style={{ display: block, textAlign: center }}
+              style={{ display: "block", textAlign: "center" }}
               data-ad-layout="in-article"
               data-ad-format="fluid"
               data-ad-client="ca-pub-1063328225356164"
