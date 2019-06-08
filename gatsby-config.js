d8f8f0adc400705be843274d0ef1cf531b86355c
tretrak <3trak10@gmail.com>
2019-06-08 16:56:32 +0100

    add robot

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,7 @@ module.exports = {
     "gatsby-plugin-sass",
     "gatsby-plugin-sharp",
     `gatsby-plugin-sitemap`,
+    "gatsby-plugin-robots-txt",
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
