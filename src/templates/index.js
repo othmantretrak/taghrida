aef52bb689ba859a0990d040b347af6ebc71ffa9
tretrak <3trak10@gmail.com>
2020-04-11 16:09:37 +0100

    add helmet3

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import useScript from "../hooks/useScript"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
