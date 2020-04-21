406c0779fd164d5443116f64951cf557af4d9c56
tretrak <3trak10@gmail.com>
2020-04-21 03:43:28 +0000

    adsense plugin9

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,13 +4,13 @@ import Header from "./header"
 import Footer from "./footer"
 import "../styles/index.scss"
 //import layoutStyles from "./layout.module.scss"
-import useScript from "../hooks/useScript"
+//import useScript from "../hooks/useScript"
 
 const Layout = props => {
-  useScript(
+  /*  useScript(
     "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
     "head"
-  )
+  ) */
   return (
     <div className="main">
       <div className="container">
