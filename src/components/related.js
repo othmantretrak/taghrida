0e13e018cf8a6e2091e04d688e37ab69661d503a
tretrak <3trak10@gmail.com>
2020-05-08 11:23:53 +0000

    edit img related

--- src/components/related.js
+++ src/components/related.js
@@ -8,7 +8,24 @@ const RelatedPost = ({ related }) => (
       {related.map(post => (
         <div key={post.slug} className="card">
           <Link to={`/${post.slug}`}>
-            <img src={post.imgUri} alt={post.title} />
+            <picture>
+              <source
+                media="(min-width: 1200px)"
+                srcSet={`${post.imgUri}?w=800&fit=fill&fm=webp`}
+                type="image/webp"
+              />
+              <source
+                media="(min-width: 992px)"
+                srcSet={`${post.imgUri}?w=600&fit=fill&fm=webp`}
+                type="image/webp"
+              />
+
+              <img
+                src={`${post.imgUri}?w=400&fit=fill&fm=webp`}
+                alt={post.title}
+              />
+            </picture>
+
             <h2>{post.title}</h2>
           </Link>
         </div>
