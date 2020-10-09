e690d0e867ccd03093273c782a1da3101c1e8d59
tretrak <3trak10@gmail.com>
2020-10-09 14:16:50 +0100

    optimisedd

--- src/components/BannerAd.js
+++ src/components/BannerAd.js
@@ -7,9 +7,11 @@ const BannerAd = () => {
   return (
     <ins
       className="adsbygoogle"
-      style={{ display: "inline-block", width: "320px", height: "52px" }}
+      style={{ display: "inline-block", height: "92px" }}
       data-ad-client="ca-pub-1063328225356164"
       data-ad-slot="1862075576"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
     ></ins>
   )
 }
